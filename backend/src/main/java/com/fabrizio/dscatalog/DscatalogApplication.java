b60aaf9c1035ba5602e62e58d025dfd11d162493
Fabrizio Abreu <fabrizioabreu@live.com>
2021-06-02 19:08:06 -0300

    Criando Projeto

---
+++ backend/src/main/java/com/fabrizio/dscatalog/DscatalogApplication.java
@@ -0,0 +1,14 @@
+package com.fabrizio.dscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscatalogApplication.class, args);
+	}
+
+	int x = 0;
+}
