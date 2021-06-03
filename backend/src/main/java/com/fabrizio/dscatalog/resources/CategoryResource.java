75f54e706e9808393168e7a9ad2ab18f6348ea35
Fabrizio Abreu <fabrizioabreu@live.com>
2021-06-03 19:35:37 -0300

    Incluindo camada DTO para Category

--- backend/src/main/java/com/fabrizio/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/fabrizio/dscatalog/resources/CategoryResource.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.fabrizio.dscatalog.dto.CategoryDTO;
 import com.fabrizio.dscatalog.entities.Category;
 import com.fabrizio.dscatalog.services.CategoryService;
 
@@ -19,8 +20,8 @@ public class CategoryResource {
 	private CategoryService service;
 
 	@GetMapping
-	public ResponseEntity<List<Category>> findall() {
-		List<Category> list = service.findAll();
+	public ResponseEntity<List<CategoryDTO>> findall() {
+		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
