e7993cfd9f762161035888572ba5e44e2e4fadab
Fabrizio Abreu <fabrizioabreu@live.com>
2021-06-07 19:51:45 -0300

    Criando Paginação

--- backend/src/main/java/com/fabrizio/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/fabrizio/dscatalog/resources/CategoryResource.java
@@ -1,9 +1,11 @@
 package com.fabrizio.dscatalog.resources;
 
-import java.util.List;
 import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.fabrizio.dscatalog.dto.CategoryDTO;
@@ -26,8 +29,18 @@ public class CategoryResource {
 	private CategoryService service;
 
 	@GetMapping
-	public ResponseEntity<List<CategoryDTO>> findall() {
-		List<CategoryDTO> list = service.findAll();
+	public ResponseEntity<Page<CategoryDTO>> findall(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+			
+			){
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		Page<CategoryDTO> list = service.findAllPaged(pageRequest);
+		
 		return ResponseEntity.ok().body(list);
 	}
 	
