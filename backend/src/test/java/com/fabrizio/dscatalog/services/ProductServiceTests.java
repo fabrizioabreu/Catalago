4ad1ddcca51e7c0edff27cd0051de0b0ffb032ae
Fabrizio Abreu <fabrizioabreu@live.com>
2021-06-23 21:01:22 -0300

    Teste no ProductService: simulando comportamento com Mockito

--- backend/src/test/java/com/fabrizio/dscatalog/services/ProductServiceTests.java
+++ backend/src/test/java/com/fabrizio/dscatalog/services/ProductServiceTests.java
@@ -1,11 +1,49 @@
 package com.fabrizio.dscatalog.services;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.fabrizio.dscatalog.repositories.ProductRepository;
 
 @ExtendWith(SpringExtension.class)
 public class ProductServiceTests {
 
+	@InjectMocks
+	private ProductService service;
 	
+	// Quando se cria um mock, não pode esquecer de configurar o comportamento simulado dele.
+	@Mock
+	private ProductRepository repository;
+	
+	private long existingId;
+	private long nonExistingId;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		existingId = 1L;
+		nonExistingId = 2L;
+		
+		// Configurar o comportamento simulado do repository usando MOCKITO
+		 Mockito.doNothing().when(repository).deleteById(existingId);
+		 
+		 Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
+	}
+	
+	@Test
+	public void deletDeveriaNaoFazerNadaQuandoExistirId() {
+		
+		Assertions.assertDoesNotThrow(() -> {
+			service.delete(existingId);
+		});
+		
+		Mockito.verify(repository, Mockito.times(1)).deleteById(existingId);
+	}
 	
 }
