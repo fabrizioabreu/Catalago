1d74b602c046eae0230c362d59942f3f1e3c0907
Fabrizio Abreu <fabrizioabreu@live.com>
2021-06-25 23:31:07 -0300

    Criando Testes com JUnit - MockBean para Product Resource Tests

---
+++ backend/src/test/java/com/fabrizio/dscatalog/resources/ProductResourceTests.java
@@ -0,0 +1,187 @@
+package com.fabrizio.dscatalog.resources;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fabrizio.dscatalog.dto.ProductDTO;
+import com.fabrizio.dscatalog.services.ProductService;
+import com.fabrizio.dscatalog.services.exceptions.DatabaseException;
+import com.fabrizio.dscatalog.services.exceptions.ResourceNotFoundException;
+import com.fabrizio.dscatalog.tests.Factory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@WebMvcTest(ProductResource.class)
+public class ProductResourceTests {
+
+	// Utilizado para fazer requisições chamando o endPoint
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@MockBean
+	private ProductService service;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
+	
+	private Long existingId;
+	private Long nonExistingId;
+	private Long dependentId;
+	private ProductDTO productDTO;
+	private PageImpl<ProductDTO> page;	// PageImpl = Para instanciar um obj concreto.
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		
+		existingId = 1L;
+		nonExistingId = 2L;
+		dependentId = 3L;
+		
+		productDTO = Factory.createProductDTO();
+		page = new PageImpl<>(List.of(productDTO));
+		
+		when(service.findAllPaged(any())).thenReturn(page);
+		
+		when(service.findById(existingId)).thenReturn(productDTO);
+		when(service.findById(nonExistingId)).thenThrow(ResourceNotFoundException.class);
+		
+		when(service.update(eq(existingId), any())).thenReturn(productDTO);
+		when(service.update(eq(nonExistingId), any())).thenThrow(ResourceNotFoundException.class);
+		
+		when(service.insert(any())).thenReturn(productDTO);
+		
+		doNothing().when(service).delete(existingId);
+		doThrow(ResourceNotFoundException.class).when(service).delete(nonExistingId);
+		doThrow(DatabaseException.class).when(service).delete(dependentId);
+		
+	}
+	
+	@Test
+	public void deletDeveriaRetornarNoDependentId() throws Exception {
+		
+		mockMvc.perform(delete("/products/{id}", dependentId)
+				.accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isBadRequest());
+	}
+	
+	@Test
+	public void deletDeveriaRetornarNoContent204QuandoExistirId() throws Exception {
+		
+		mockMvc.perform(delete("/products/{id}", existingId)
+				.accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isNoContent());
+	}
+	
+	@Test
+	public void deletDeveriaRetornarNotFound204QuandoNaoExistirId() throws Exception {
+		
+		mockMvc.perform(delete("/products/{id}", nonExistingId)
+				.accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isNotFound());
+	}
+	
+	@Test
+	public void insertDeveriaRetornarCreated201ComProductDTO() throws Exception {
+		
+		String jsonBody = objectMapper.writeValueAsString(productDTO);
+		
+		mockMvc.perform(post("/products")
+				.content(jsonBody)
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isCreated())
+		.andExpect(jsonPath("$.id").exists())
+		.andExpect(jsonPath("$.name").exists())
+		.andExpect(jsonPath("$.description").exists());
+	}
+	
+	@Test
+	public void updateDeveriaRetornarUmProductDTOQuandoExistirId() throws Exception {
+		
+		String jsonBody = objectMapper.writeValueAsString(productDTO);
+		
+		mockMvc.perform(put("/products/{id}", existingId)
+				.content(jsonBody)
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk())
+		.andExpect(jsonPath("$.id").exists())
+		.andExpect(jsonPath("$.name").exists())
+		.andExpect(jsonPath("$.description").exists());
+	}
+	
+	@Test
+	public void updateDeveriaRetornarNotFoundQuandoNaoExistirId() throws Exception {
+		
+		String jsonBody = objectMapper.writeValueAsString(productDTO);
+		
+		mockMvc.perform(put("/products/{id}", nonExistingId)
+				.content(jsonBody)
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isNotFound());
+	}
+	
+	@Test
+	public void findAllDeveriaRetornarUmPage() throws Exception {  
+		mockMvc.perform(get("/products")
+				.accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk());
+	}
+	
+	@Test
+	public void findByIdDeveriaRetornarUmProductDTOQuandoExistirId() throws Exception {
+		
+		mockMvc.perform(get("/products/{id}", existingId)
+				.accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk())
+		.andExpect(jsonPath("$.id").exists())
+		.andExpect(jsonPath("$.name").exists())
+		.andExpect(jsonPath("$.description").exists());
+	}
+	
+	@Test
+	public void findByIdDeveriaRetornarNotFoundQuandoNaoExistirId() throws Exception {
+		
+		mockMvc.perform(get("/products/{id}", nonExistingId)
+				.accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isNotFound());
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
