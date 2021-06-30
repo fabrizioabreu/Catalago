13f856fed87d866945e9687a29c01200b79d6967
Fabrizio Abreu <fabrizioabreu@live.com>
2021-06-30 12:26:51 -0300

    User Update Validator

--- backend/src/main/java/com/fabrizio/dscatalog/resources/UserResource.java
+++ backend/src/main/java/com/fabrizio/dscatalog/resources/UserResource.java
@@ -20,6 +20,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.fabrizio.dscatalog.dto.UserDTO;
 import com.fabrizio.dscatalog.dto.UserInsertDTO;
+import com.fabrizio.dscatalog.dto.UserUpdateDTO;
 import com.fabrizio.dscatalog.services.UserService;
 
 @Controller
@@ -49,9 +50,9 @@ public class UserResource {
 	}
 
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<UserDTO> update(@PathVariable Long id, @Valid @RequestBody UserDTO dto) {
-		dto = service.update(id, dto);
-		return ResponseEntity.ok().body(dto);
+	public ResponseEntity<UserDTO> update(@PathVariable Long id, @Valid @RequestBody UserUpdateDTO dto) {
+		UserDTO newDto = service.update(id, dto);
+		return ResponseEntity.ok().body(newDto);
 	}
 
 	@DeleteMapping(value = "/{id}")
--- backend/src/main/java/com/fabrizio/dscatalog/services/UserService.java
+++ backend/src/main/java/com/fabrizio/dscatalog/services/UserService.java
@@ -16,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.fabrizio.dscatalog.dto.RoleDTO;
 import com.fabrizio.dscatalog.dto.UserDTO;
 import com.fabrizio.dscatalog.dto.UserInsertDTO;
+import com.fabrizio.dscatalog.dto.UserUpdateDTO;
 import com.fabrizio.dscatalog.entities.Role;
 import com.fabrizio.dscatalog.entities.User;
 import com.fabrizio.dscatalog.repositories.RoleRepository;
@@ -58,7 +59,7 @@ public class UserService {
 	}
 
 	@Transactional
-	public UserDTO update(Long id, UserDTO dto) {
+	public UserDTO update(Long id, UserUpdateDTO dto) {
 		try {
 			User entity = repository.getOne(id);
 			copyDtoToEntity(dto, entity);
